e3248ae431ad27b4f158b17d5c0f28306b51aad7
Natalia Lima <natalima.al@gmail.com>
2022-08-02 17:09:24 -0300

    adicionando metodos

--- Admin.js
+++ Admin.js
@@ -4,8 +4,11 @@ class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
         super(nome, email, nascimento, role, ativo)
     }
+
+    criarCurso(nomeDoCurso, vagas) {
+        return `Curso de ${nomeDoCurso} criado com ${vagas}`
+    }
 }
 
 const novoAdmin = new Admin('Amanda', 'a@a.com', '2022-01-01')
-console.log(novoAdmin)
-console.log(novoAdmin.exibirInfos())
+console.log(novoAdmin.criarCurso('JS', 20))
