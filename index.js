a7b380ce1b3c545c19a89bd5e9e0715518ee9a07
Natalia Lima <natalima.al@gmail.com>
2022-08-02 18:03:07 -0300

    metodos privados

--- index.js
+++ index.js
@@ -3,6 +3,4 @@ import Docente from "./Docente.js";
 import Admin from "./Admin.js";
 
 const novoUser = new User('Mariana', 'm@m.com', '2022-01-01')
-console.log(novoUser.exibirInfos())
-
-novoUser.#email = 'x@x.com'
+console.log(novoUser.#montaObjUser())
